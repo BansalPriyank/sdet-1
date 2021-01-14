39cc14f3c6cde1251107dc8cc1379d3dc3ea9f7a
BansalPriyank <76939825+BansalPriyank@users.noreply.github.com>
2021-01-14 17:20:15 +0530

    Add files via upload

---
+++ python/Activity6.py
@@ -0,0 +1,3 @@
+# use for loop to iterate 10 times
+for i in range(10):
+    print(str(i) * i)
---
+++ python/Activity7.py
@@ -0,0 +1,7 @@
+
+numList = [10, 20, 30, 40, 10]
+sum = 0
+
+for num in numList:
+  sum += int(num)
+print(sum)
